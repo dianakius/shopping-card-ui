15309bb1499f81cd19ba3cb14794fb034630ad84
dianakius <eingidiana@gmail.com>
2025-11-18 21:15:08 +0200

    feat: useState and useEffect and import ProductList

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,36 @@
-const App = () => {
-  return (
-    <div>Shopping Card UI</div>
-  )
-}
+import { useState, useEffect } from "react";
+import ProductList from "./components/ProductList";
 
-export default App
+const App = () => {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState([true]);
+  const [error, setError] = useState([null]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const res = await fetch("http://localhost:8000/products");
+        if (!res.ok) throw new Error("Network response was not ok");
+        const data = await res.json();
+        console.log(data);
+        setProducts(data);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchProducts();
+  }, []);
+
+  return (
+    <div className="min-h-screen bg-gray-100 p-6">
+      <h1 className="text-3xl font-bold mb-6">Product Catalog</h1>
+      {loading && <p>Loading products...</p>}
+      {error && <div className="text-red-500">Error: {error}</div>}
+   <ProductList products={products} />
+    </div>
+  );
+};
+
+export default App;
