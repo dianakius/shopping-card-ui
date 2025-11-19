0831cc815380d18ae941118af2533fdbfe20880f
dianakius <eingidiana@gmail.com>
2025-11-19 15:27:54 +0200

    feat: import CartProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import ProductProvider from './context/ProductContext.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+import { ProductProvider } from "./providers/ProductProvider.jsx";
+import { CartProvider } from "./providers/CartProvider.jsx";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ProductProvider>
-    <App />
+      <CartProvider>
+        <App />
+      </CartProvider>
     </ProductProvider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
