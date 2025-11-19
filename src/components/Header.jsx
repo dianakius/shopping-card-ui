09f5b87e8c8ed1181cf416f7ec60e430e0b35805
dianakius <eingidiana@gmail.com>
2025-11-19 15:51:59 +0200

    feat: create Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,26 @@
+import { useCart } from "../hooks/useCart";
+import { FaShoppingCart } from "react-icons/fa";
+
+const Header = () => {
+  const { cart } = useCart();
+  const itemCount = cart.reduce((acc, item) => acc + item.qty, 0);
+
+  return (
+    <header className="bg-white shadow-md p-4 flex justify-between  items-center">
+      <h1 className="text-2xl font-bold   text-blue-600">ShopEasy</h1>
+        <div className="relative">
+          <FaShoppingCart className="text-2xl text-green-600" />
+{
+    itemCount > 0 && (
+        <span className='absolute -top-2 -right-2 bg-red-500 text-white text-xs w-5 h-5 flex items-center justify-center rounded-full'>
+          {itemCount}
+        </span> 
+    )
+}
+
+          </div>
+    </header>
+  );
+};
+
+export default Header;
