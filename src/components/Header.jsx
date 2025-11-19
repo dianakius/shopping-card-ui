d0a06623116dd3ec1418cf39db8414384a1bad6e
dianakius <eingidiana@gmail.com>
2025-11-19 16:28:16 +0200

    feat: implement cart Items dropdown

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,58 @@
 import { useCart } from "../hooks/useCart";
 import { FaShoppingCart } from "react-icons/fa";
+import { useState } from "react";
 
 const Header = () => {
+  const [showDropdown, setShowDropdown] = useState(false);
+
   const { cart } = useCart();
+
   const itemCount = cart.reduce((acc, item) => acc + item.qty, 0);
+  const total = cart.reduce((acc, item) => acc + item.qty, 0).toFixed(2);
 
   return (
     <header className="bg-white shadow-md p-4 flex justify-between  items-center">
       <h1 className="text-2xl font-bold   text-blue-600">ShopEasy</h1>
-        <div className="relative">
+      <div className="relative">
+        <button
+          className="cursor-pointer"
+          onClick={() => setShowDropdown(!showDropdown)}
+        >
           <FaShoppingCart className="text-2xl text-green-600" />
-{
-    itemCount > 0 && (
-        <span className='absolute -top-2 -right-2 bg-red-500 text-white text-xs w-5 h-5 flex items-center justify-center rounded-full'>
-          {itemCount}
-        </span> 
-    )
-}
+          {itemCount > 0 && (
+            <span className="absolute -top-2 -right-2 bg-red-500 text-white text-xs w-5 h-5 flex items-center justify-center rounded-full">
+              {itemCount}
+            </span>
+          )}
+        </button>
 
+        {showDropdown && (
+          <div className="absolute right-0 mt-2 w-80 bg-white border rounded shadow-lg z-50">
+            <div className="p-4">
+              <h2 className="font-semibold text-lg mb-2">Your Cart Items</h2>
+              {cart.length === 0 ? (
+                <p className="text-gray-500">Your cart is empty.</p>
+              ) : (
+                <>
+                  <ul className="max-h-60 overflow-y-auto divide-y divide-gray-200">
+                    {cart.map((item) => (
+                      <li
+                        key={item.id}
+                        className="py-2 flex justify-between items-center"
+                      >
+                        <div>
+                          <p className="font-semibold">{item.name}</p>
+                          <p className='text-sm     text-gray-600'>{item.qty} x ${item.price} </p>
+                        </div>
+                      </li>
+                    ))}
+                  </ul>
+                </>
+              )}
+            </div>
           </div>
+        )}
+      </div>
     </header>
   );
 };
