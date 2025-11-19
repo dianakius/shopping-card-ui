0bbae2479a9a40a58654ee8be69782c3e5f36bef
dianakius <eingidiana@gmail.com>
2025-11-19 12:42:10 +0200

    fix: change the fetch address

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ const App = () => {
   useEffect(() => {
     const fetchProducts = async () => {
       try {
-        const res = await fetch("http://localhost:8000/products");
+        const res = await fetch('/api/products');
         if (!res.ok) throw new Error("Network response was not ok");
         const data = await res.json();
 
