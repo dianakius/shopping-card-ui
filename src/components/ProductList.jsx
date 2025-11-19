eee693845faa64c3500aa043887a90501a1dc1ca
dianakius <eingidiana@gmail.com>
2025-11-19 12:58:05 +0200

    feat: import useProducts component

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,13 +1,20 @@
 import ProductCard from "./ProductCard";
+import {useProducts} from "../context/ProductContext.jsx";
 
-const ProductList = ({products}) => {
+const ProductList = () => {
+  const { products, loading, error } = useProducts();
+
+  if (loading) return <p>Loading products...</p>;
+  if (error) return <div className="text-red-500"> {error}</div>;
   return (
-       <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-        {products.map((product) => (
-          <ProductCard key={product.id} product={product} />
-        ))}
-      </div>
-  )
-}
+    <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+      {loading && <p>Loading products...</p>}
+      {error && <div className="text-red-500"> {error}</div>}
+      {products.map((product) => (
+        <ProductCard key={product.id} product={product} />
+      ))}
+    </div>
+  );
+};
 
-    export default ProductList;
+export default ProductList;
