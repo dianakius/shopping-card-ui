490350d6e15f073558cfb91f78e7654a89103841
dianakius <eingidiana@gmail.com>
2025-11-19 15:28:36 +0200

    fix: add loading and error

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,15 +1,14 @@
 import ProductCard from "./ProductCard";
-import {useProducts} from "../context/ProductContext.jsx";
+import { useProducts } from "../hooks/useProducts";
 
 const ProductList = () => {
   const { products, loading, error } = useProducts();
 
   if (loading) return <p>Loading products...</p>;
-  if (error) return <div className="text-red-500"> {error}</div>;
+  if (error) return <div className="text-red-500">{error}</div>;
+
   return (
     <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-      {loading && <p>Loading products...</p>}
-      {error && <div className="text-red-500"> {error}</div>}
       {products.map((product) => (
         <ProductCard key={product.id} product={product} />
       ))}
